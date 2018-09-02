ec4432064a26820bcc0089a2ab8636cfa5d721da
NAN JIANG <nj499521010@gmail.com>
2018-09-02 20:26:11 +1200

    Doing myblog

--- blog/myblog/index.js
+++ blog/myblog/index.js
@@ -1,13 +1,42 @@
-const express = require('express');
 const path = require('path');
-const app = express();
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const config = require('config-lite')(__dirname);
+const routes = require('./routes');
+const pkg = require('./package');
 
-app.set('/views', path.join(__dirname, 'vies'));// 模板存放目录
+const app = express();
+
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为ejs
 app.set('view engine', 'ejs');
 
-app.use('/', indexRouter);
-app.use('/users', userRouter);
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+// session 中间件
+app.use(session({
+    name: config.session.key, // 设置cookie中保存session id 的字段名称
+    secret: config.session.secret, // 设置secret来计算hash值并放在cookie中, 使产生的signedCookie 防篡改
+    resave: true, // 强制更新session
+    saveUninitialized: false, // 设置为false, 强制创建一个session, 即使用户未登录
+    cookie: {
+        maxAge: config.session.maxAge // 过期时间, 过期后cookie中的session id 自动删除
+    },
+    store: new MongoStore({ // 将session 存到mongodb
+        url: config.mongodb // mongodb 的地址
+    })
+}));
 
-app.listen(3000);
+// flash中间件, 用来显示通知
+app.use(flash);
+
+// 路由
+routes(app);
+
+// 监听端口, 启动程序
+app.listen(config.port, function() {
+    console.log(`${pkg.name} listening on port ${config.port}`);
+});
