ecabbe85e9067a5fd2298a940c8dbbe0de008bf9
NanJiang327 <nj499521010@gmail.com>
2018-10-04 14:24:37 +1300

    Separate projects

--- movie/app/models/users.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = require('../schemas/users');
-const User = mongoose.model('User', UserSchema);
-
-module.exports = User;
--- movie/config/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 3000,
-    session: {
-        secret: 'nan',
-        key: 'nan',
-        maxAge: 2592000000,
-    },
-    mongodb: 'mongodb://localhost:27017/nan',
-}
